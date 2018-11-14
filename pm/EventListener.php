a99859399a6363458aab34f640150edfb47330f9
Christian Mollekopf <mollekopf@kolabsys.com>
2018-11-14 12:10:12 +0100

    Added EventListener stub.
    I don't get why this isn't part of the API,
    but I need it so I can run phpstan

---
+++ pm/EventListener.php
@@ -0,0 +1,7 @@
+<?php
+// Copyright 1999-2017. Plesk International GmbH.
+class EventListener
+{
+    public function filterActions() {}
+    public function handleEvent($objectType, $objectId, $action, $oldValue, $newValue) {}
+}
