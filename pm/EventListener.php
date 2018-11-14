d374571130087cd724939c69d1534d97b1ce138f
Christian Mollekopf <mollekopf@kolabsys.com>
2018-11-14 14:25:35 +0100

    EventListener is an interface

--- pm/EventListener.php
+++ pm/EventListener.php
@@ -1,7 +1,7 @@
 <?php
 // Copyright 1999-2017. Plesk International GmbH.
-class EventListener
+interface EventListener
 {
-    public function filterActions() {}
-    public function handleEvent($objectType, $objectId, $action, $oldValue, $newValue) {}
+    public function filterActions();
+    public function handleEvent($objectType, $objectId, $action, $oldValue, $newValue);
 }
